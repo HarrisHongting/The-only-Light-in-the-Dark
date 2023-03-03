8d098482440faac0eaa41466409babe8712d6fca
呀一西巴老马\lhtbj <lhtbj123456789@gmail.com>
2023-03-03 02:39:05 +0000

    实现了monster的追逐以及3d音效

---
+++ Assets/Scripts/MonsterController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MonsterController : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 5f;
+    public float stoppingDistance = 1f;
+
+    private void Update()
+    {
+        // 计算怪物和玩家之间的距离
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        // 如果怪物在玩家后面，就向玩家移动
+        if (distance > stoppingDistance)
+        {
+            //Debug.Log(distance);
+            transform.LookAt(player); 
+            //Vector3 direction = (player.position - transform.position).normalized;
+            //transform.Translate(direction * moveSpeed * Time.deltaTime, Space.World);
+            transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+        }
+        // 如果怪物追到了玩家，就结束游戏
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
